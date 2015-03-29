a121d106a119f4e16be98da7d84fb3242ccd7aea
John Woodruff <johnwoodruff91@gmail.com>
2015-03-29 00:32:00 -0600

    Structure income and expense pages, begin basic structure of summary page

--- www/js/controllers/expensectrl.js
+++ www/js/controllers/expensectrl.js
@@ -1,5 +1,52 @@
 angular.module('starter.controllers')
 
-.controller('ExpenseCtrl', ['$scope', function($scope) {
+.controller('ExpenseCtrl', ['$scope', '$ionicPopup', function($scope, $ionicPopup) {
 
+	$scope.addExpense = function() {
+		$ionicPopup.alert({
+			title: 'Success',
+			template: 'Added expense.'
+		});
+		resetExpense();
+	};
+
+	function resetExpense() {
+		$scope.expense = {
+			"id":0,
+			"amount":null,
+			"date":null,
+			"item":"",
+			"category":$scope.categories[0]
+		};
+	}
+
+	// Set up controller initially
+	function ctrlSetup() {
+		// TODO: Get from DB instead of hardcoded
+		$scope.categories = [
+			{
+				"id":0,
+				"description":"Uncategorized"
+			},
+			{
+				"id":1,
+				"description":"Fabric"
+			},
+			{
+				"id":2,
+				"description":"Thread"
+			},
+			{
+				"id":3,
+				"description":"Pattern"
+			},
+			{
+				"id":4,
+				"description":"Misc"
+			}
+		];
+		resetExpense();
+	}
+
+	ctrlSetup();
 }]);
