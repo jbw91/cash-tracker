65cbaf9e802763a163464c9ed537328f90314378
John Woodruff <johnwoodruff91@gmail.com>
2015-04-19 18:58:21 -0600

    Add Feedback Email Functionality

--- www/js/controllers/settingsctrl.js
+++ www/js/controllers/settingsctrl.js
@@ -1,5 +1,15 @@
 angular.module('starter.controllers')
 
-.controller('SettingsCtrl', ['$scope', 'Categories', '$rootScope', function($scope, Categories, $rootScope) {
-	
+.controller('SettingsCtrl', ['$scope', 'Categories', '$rootScope', '$window', '$ionicPopup', function($scope, Categories, $rootScope, $window, $ionicPopup) {
+	$scope.giveFeedback = function() {
+		cordova.plugins.email.open({
+			to: 'woodruffapps@gmail.com',
+			subject: 'Cash Tracker Feedback'
+		}, function() {
+			$ionicPopup.alert({
+				title: 'Thank You',
+				template: 'Your feedback has been sent to the developer.'
+			});
+		}, this);
+	};
 }]);
